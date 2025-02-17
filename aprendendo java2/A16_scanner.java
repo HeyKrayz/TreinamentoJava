1c46700dff3a5d673ad217c6d3fa8b132a82011b
krayz <denissilvestre007@gmail.com>
2025-02-16 22:57:13 -0300

    Add files via upload

---
+++ aprendendo java2/A16_scanner.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class A16_scanner {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.println ("user: ");
+        String nome = scanner.nextLine();
+        int idade = scanner.nextInt();
+
+    }
+}
