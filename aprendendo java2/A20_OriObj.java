22c84ccd4e8e0934ce2259069641898178b9641c
krayz <denissilvestre007@gmail.com>
2025-02-23 01:41:17 -0300

    Add files via upload

---
+++ aprendendo java2/A20_OriObj.java
@@ -0,0 +1,7 @@
+public class A20_OriObj {
+    public static void main (String[] args) {
+        pessoa pessoa = new pessoa();
+        
+        System.out.println ("nome: "+ pessoa.nome + " idade: "+ pessoa.idade + " sexo: "+ pessoa.sexo);
+    }
+}
---
+++ aprendendo java2/A20_Pessoa.java
@@ -0,0 +1,5 @@
+public class pessoa {
+    public String nome = "krayz";
+    public int idade = 16;
+    public char sexo = 'm';
+}
